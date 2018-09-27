1bda40d327362c57111502df8fd807ca1278fa0d
eet172840 <ravisinghthakur1115@gmail.com>
2018-09-27 07:45:41 +0530

    commit-1 for ps1

--- ps1.py
+++ ps1.py
@@ -1,3 +1,25 @@
 ###### this is the first .py file ###########
 
-####### write your code here ##########
+#importing array package from python library
+from array import *
+
+#taking input from user for dimensions of matrix
+n=int(input())
+m=int(input())
+
+#initializing array to store values
+arr = []
+for i in range(n):
+    arr.append([])
+    for j in range(m):
+    	#appending either D or S in array
+        arr[i].append(input())
+
+
+#This loop is printing input taken in desired format
+for r in arr:
+    for c in r:
+        print(c,end = "")
+    print()
+
+#Now searching for pattern
